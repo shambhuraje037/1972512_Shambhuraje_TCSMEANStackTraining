567d4befab09b54d3f4f61fb8a0ee34638fa0bbd
shambhuraje <shambhuraje037@gmail.com>
2021-04-05 21:48:31 -0500

    Log Record updated

--- LogRecords/main.js
+++ LogRecords/main.js
@@ -2,11 +2,5 @@
 
 // //rec.clearRecord() // clear all entries from record
 
-// //rec.addEntry() // add a single entry
-
-// rec.addMultipleEntries() // add multiple entries
-
-
-
 let obj = require('./records')          
 obj.logRecord()
